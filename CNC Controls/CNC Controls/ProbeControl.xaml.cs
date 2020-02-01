93ad0718fd8c60135056245df92fcadc58c9db23
Jesse Schoch <schoch@gmail.com>
2020-02-01 14:00:10 -0800

    doh, forgot files

---
+++ CNC Controls/CNC Controls/ProbeControl.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using CNC.Core;
+
+namespace CNC.Controls
+{
+    /// <summary>
+    /// Interaction logic for ProbeControl.xaml
+    /// </summary>
+    /// 
+
+    public partial class ProbeControl : UserControl
+    {
+        //private double distance = 1d, feedrate = 500d;
+        private bool initOK = false, sdStream = false;
+        private GrblViewModel model;
+        public ProbeControl()
+        {
+            InitializeComponent();
+            
+            DataContextChanged += View_DataContextChanged;
+        }
+        private void View_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.OldValue != null && e.OldValue is INotifyPropertyChanged)
+                ((INotifyPropertyChanged)e.OldValue).PropertyChanged -= OnDataContextPropertyChanged;
+            if (e.NewValue != null && e.NewValue is INotifyPropertyChanged)
+                (e.NewValue as GrblViewModel).PropertyChanged += OnDataContextPropertyChanged;
+        }
+
+        private void OnDataContextPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (sender is GrblViewModel && Visibility == Visibility.Visible) switch (e.PropertyName)
+                {
+                    case nameof(GrblViewModel.StreamingState):
+                        if ((sender as GrblViewModel).IsJobRunning)
+                            Visibility = Visibility.Hidden;
+                        break;
+
+                    case nameof(GrblViewModel.ActiveView):
+                        if ((sender as GrblViewModel).ActiveView != View.ViewType.GRBL)
+                            Visibility = Visibility.Hidden;
+                        break;
+                }
+        }
+
+        public void clickStartProbeButton(object sender, RoutedEventArgs e, string s)
+        {
+            string cmd = "$J=G91 x1";
+            //(model.data as GrblViewModel).ExecuteMDI(cmd);
+
+            
+            MessageBox.Show($"cmd: {cmd} {sender} {e} {s}");
+            (DataContext as GrblViewModel).ExecuteMDI(cmd);
+        }
+    }
+}
---
+++ GCode Sender/GCode Sender/ProbeView.xaml.cs
@@ -0,0 +1,57 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+
+using CNC.View;
+using CNC.Core;
+
+using System.ComponentModel;
+
+using CNC.Controls;
+
+
+namespace GCode_Sender
+{
+    /// <summary>
+    /// Interaction logic for Probe.xaml
+    /// </summary>
+    public partial class ProbeView : UserControl, CNCView
+    {
+        private bool initOK = false, sdStream = false;
+        private GrblViewModel model;
+        //public string zProbeDistance = "0.0";
+        private ProbeControl pc;
+        
+        public ProbeView()
+        {
+            InitializeComponent();
+            pc = new ProbeControl();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        #region Methods and properties required by CNCView interface
+
+        public ViewType mode { get { return ViewType.AppConfig; } }
+
+        public void Activate(bool activate, ViewType chgMode)
+        {
+            
+        }
+
+        private void clickStartProbeButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            pc.clickStartProbeButton(sender, e, zProbeDistance.Text);
+        }
+
+        public void CloseFile()
+        {
+        }
+
+        #endregion
+    }
+
+}
